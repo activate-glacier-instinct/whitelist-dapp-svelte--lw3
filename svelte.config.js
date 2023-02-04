01962d27191bbc6a319548b7f8ce20ab2d27888b
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-04 02:22:06 +0000

    FIX (ci/cd) : changed the appDir

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 			strict: true,
 			// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
 			// For example, instead of '_app', use 'app_', 'internal', etc.
-			appDir: 'internal',
+			appDir: '_app',
 			prerender: { entries: ["*"] },
 			paths: {
 				base: dev ? '' : '/whitelist-dapp-svelte--lw3',
