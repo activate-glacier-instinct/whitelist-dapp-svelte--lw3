27af0aa25b9eba59f182d4c8c65d55165949b116
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-04 01:56:30 +0000

    FEAT (build) : adds static building config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,22 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'out',
+			assets: 'out',
+			fallback: null,
+			precompress: true, // brotli and gzip
+			strict: true,
+			// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+			// For example, instead of '_app', use 'app_', 'internal', etc.
+			appDir: 'internal',
+			prerender: { entries: ["*"] },
+			paths: {
+				base: dev ? '' : '/whitelist-dapp-svelte--lw3',
+			},
+		})
 	}
 };
 
